2a6b877d72526d41474ba41bde42a55bf9b3d9b1
amina-tech-stack <amina.djellal@etu.usthb.dz>
2024-10-18 15:59:34 +0100

    make payment summary interractive

--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -4,6 +4,7 @@ import { formatCurrency } from '../utils/money.js';
 import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import { deliveryOptions ,getDeliveryOption} from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 hello();
 export function renderOrderSummary(){
@@ -105,6 +106,8 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
 
         const container = document.querySelector(`.js-cart-item-container-${productId}`);
         if (container) container.remove();
+        renderOrderSummary();
+        renderPaymentSummary();
     });
 });
 
@@ -114,6 +117,7 @@ document.querySelectorAll('.js-delivery-option').forEach((element) => {
         const { productId, deliveryOptionId } = element.dataset;
         updateDeliveryOptions(productId, deliveryOptionId);
         renderOrderSummary();// model > view mvc technic
+        renderPaymentSummary();
     });
 });
 }
