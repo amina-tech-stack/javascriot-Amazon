0321ed58e73431deb3121038218f2a9a69f17b77
amina-tech-stack <amina.djellal@etu.usthb.dz>
2024-10-25 19:12:29 +0100

    Load products from the backend

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,6 +1,11 @@
 import {cart, addToCart} from '../data/cart.js'
-import {products} from '../data/products.js'
+import {products,loadProducts} from '../data/products.js'
 import { formatCurrency } from './utils/money.js';
+
+loadProducts(renderProductsGrid);
+
+function renderProductsGrid(){
+
 let productHTML = ''; 
 products.forEach((product) => {
     productHTML += `
@@ -73,3 +78,4 @@ button.addEventListener('click', () =>{
      document.querySelector('.js-cart-quantity').innerHTML=cartQuanitity;    
 });
 });
+};
